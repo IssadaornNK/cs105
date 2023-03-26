2b283b5d0b5ab3e29f449ae147c8147fac9c4aec
IssadaornNK <111173930+Issadaorn@users.noreply.github.com>
2023-03-26 12:08:38 +0700

    hjfhjkfv

---
+++ LAB#2/lab2/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className="layout">
+        <img src="img/Me.jpg" alt="profile"/>
+        <h1 className="name">Ping Kiadtikul</h1>
+        <hr></hr>
+        </div>
+    )
+}
+export default Profile; 
