1209e876872bc48c6027ac9aee37c84c5326c6f1
IssadaornNK <111173930+Issadaorn@users.noreply.github.com>
2023-03-29 22:15:01 +0700

    aaaaaa

--- assignmen10/src/App.jsx
+++ assignmen10/src/App.jsx
@@ -14,13 +14,13 @@ function App() {
       <Nav />
       <Container >
         <Grid container direction="row" spacing={2} pt={5}>
-          <Grid item xs={3}>
+          <Grid item sx={{ display: { xs: 'none', sm: 'none', md:'block' } }}>
             <Sidebar />
           </Grid>
           <Grid item xs={12} md={6}>
             <Content />
           </Grid>
-          <Grid item xs={3}>
+          <Grid item sx={{ display: { xs: 'none', sm: 'none', md:'block' } }}>
             <Rightbar />
           </Grid>
         </Grid>
