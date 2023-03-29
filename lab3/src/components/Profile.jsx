5041ed966a5755f371afb42e880ec2e62cb5c0ad
IssadaornNK <111173930+Issadaorn@users.noreply.github.com>
2023-03-29 23:12:32 +0700

    AAAAAAAA

---
+++ lab3/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className="layout">
+            <img src="assets/Neo.jpg" alt="profile" />
+            <h1 className="name">Issadaorn Kulsantao</h1>
+            <hr></hr>
+        </div>
+    )
+}
+export default Profile;
