a85c8a628ee3da878f59e2ab8ed48a92c91443c4
IssadaornNK <111173930+Issadaorn@users.noreply.github.com>
2023-04-07 13:47:26 +0700

    bug yer jung aa

---
+++ lab4/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className="layout">
+        <img src="assets/Me.jpg" alt="profile"/>
+        <h1 className="name">Neo Issadaorn</h1>
+        <hr></hr>
+        </div>
+    )
+}
+export default Profile; 
