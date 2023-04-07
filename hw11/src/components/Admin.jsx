c7a0343da86a0802f70bf03b767a684215a734c5
IssadaornNK <111173930+Issadaorn@users.noreply.github.com>
2023-04-07 13:07:19 +0700

    done1

---
+++ hw11/src/components/Admin.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/Admin.css'
+import { Navigate } from 'react-router-dom'
+
+function Admin({ userLogin, setuserLogin }) {
+    const handleLogout = () => setuserLogin(false)
+
+    if (!userLogin) {
+        return <Navigate to="/" replace />;
+    }
+
+    return (
+        <div>
+            <img src="src/assets/Pic1.jpg" alt="Pallas Cat" />
+            <h1>This is Admin Page</h1>
+            <button className='logout' onClick={handleLogout}>Logout</button>
+        </div>
+    )
+}
+
+export default Admin
